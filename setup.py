29583d1f9231db71cfd9295cf7a2bfc98d19d128
Lael Halawani <lael.halawani.qadev@gmail.com>
2023-11-09 22:40:41 +0100

    renamed samples to samples_handler

--- setup.py
+++ setup.py
@@ -5,4 +5,8 @@ setup(
    version='1.0',
    packages=find_packages(),
    python_requires='>=3.11',
+   long_description=open('README.md').read(),
+   author='Lael Al-Halawani',
+   author_email='laelhalawani@gmail.com',
+   url='https//github.com/laelhalawani/samples-handler'
 )
